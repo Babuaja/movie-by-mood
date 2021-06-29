951470b6e197339ac5e118ef9383c1200802a246
Arsal Fadilah <arsal.fadilah.tif420@polban.ac.id>
2021-06-30 00:58:45 +0700

    hufthh jadi juga dashboardnya

--- js/kode.js
+++ js/kode.js
@@ -36,3 +36,63 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
     // Your code to run since DOM is loaded and ready
 });
+
+//get modal
+var modal = document.getElementById("myModal");
+//get body for overflow (scroll) disable
+var body = document.getElementById("body-pd");
+//get image
+var img = document.getElementsByClassName('targetImg');
+//get close
+var fclose = document.getElementById("close");
+//get tittle
+var ftittle = document.getElementById("ftitle");
+//get imgModal
+var fImg = document.getElementById('fImg');
+//get details
+var details = document.getElementById("fdetails");
+//get button close
+var buttonClose=document.getElementById("bclose");
+//when image onclick
+console.log(img);
+img[0].onclick = function(){
+    modal.style.display = "block";
+    ftittle.innerHTML = 'Tittle of The Movie';
+    fImg.src = this.src;
+    details.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum metus eu leo pretium, a sagittis sem tempus. Quisque gravida est eu nibh dapibus cursus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas sed suscipit lectus. Praesent varius imperdiet felis, et lacinia lectus porttitor eu. Nullam bibendum orci ut vehicula auctor. Nulla blandit scelerisque tortor, non ultrices urna efficitur quis. In accumsan erat dapibus libero scelerisque gravida. Nunc tempor dui ut convallis ornare.<br><br>tahun : 1xxx<br>genre: xxx<br>artis: a, b, c<br>dsb';
+    body.style.overflow = "hidden";
+}
+
+img[1].onclick = function(){
+    modal.style.display = "block";
+    ftittle.innerHTML = 'Tittle of The Movie';
+    fImg.src = this.src;
+    details.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum metus eu leo pretium, a sagittis sem tempus. Quisque gravida est eu nibh dapibus cursus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas sed suscipit lectus. Praesent varius imperdiet felis, et lacinia lectus porttitor eu. Nullam bibendum orci ut vehicula auctor. Nulla blandit scelerisque tortor, non ultrices urna efficitur quis. In accumsan erat dapibus libero scelerisque gravida. Nunc tempor dui ut convallis ornare.<br><br>tahun : 1xxx<br>genre: xxx<br>artis: a, b, c<br>dsb';
+    body.style.overflow = "hidden";
+}
+
+buttonClose.onclick = function(){
+    modal.style.display = 'none';
+    body.style.overflow = 'visible';
+    console.log("masujk sad");
+}
+// // Get the modal
+// var modal = document.getElementById('myModal');
+        
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById('myImg');
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() { 
+//   modal.style.display = "none";
+// }
